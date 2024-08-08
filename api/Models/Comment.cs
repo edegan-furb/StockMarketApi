c6bf2b7bbddd7f14813d2a1f5ea1a2ad37ff8c77
edegan-furb <eduardord324@gmail.com>
2024-08-08 00:18:04 -0300

    Add project files.

---
+++ api/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // Navigation
+        public Stock? Stock { get; set; }
+
+    }
+}
